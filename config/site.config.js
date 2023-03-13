eacce283d185b9118f49f6c7bd49d873c54e71a6
Ronan <roleary81@gmail.com>
2023-03-13 08:34:06 +0100

    initial commit of template and scaffold

---
+++ config/site.config.js
@@ -0,0 +1,15 @@
+export default {
+  "metaData": {
+    "title" : "BLP Media",
+    "description" : "BLP - a modern, stylish Tailwind CSS template for any blog, magazine, or news site.",
+    "author" : "Tailwind Awesome",
+    "keyword" : "magazine, multipurpose, react, nextjs, blog, template",
+    "ogImage" : "/images/ogimage.jpg"
+  },
+  "baseURL" : "https://banter.tailwindawesome.com/",
+  "logo" : "/images/banter-logo.png",
+  "logoText" : "BLP",
+  "favicon" : "/images/banter-icon-logo.png",
+  "postPerPage" : 6,
+  "copyright" : "© 2023 BLP. All rights reserved."
+}
