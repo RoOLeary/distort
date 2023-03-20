66b27fada0392fa5c561a41aa49ba70e171621aa
Ronan <roleary81@gmail.com>
2023-03-21 00:52:34 +0100

    one more typo

--- components/shared/TestimonialSlider.js
+++ components/shared/TestimonialSlider.js
@@ -65,7 +65,7 @@ const TestimonialSlider = ({ testimonials }) => {
                 onClick={handlePrevSlide}
                 className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-l"
               >
-                Prev
+                Previous
               </button>
               <button
                 onClick={handleNextSlide}
